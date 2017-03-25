24a494a214e48f1dfb733dc9d4b92a8ad3296d44
airCstnr <raphael.castanier@free.fr>
2017-03-25 15:07:17 +0100

    Ajout des reglements, resultats, partenaires et corrections diverses

--- nav.js
+++ nav.js
@@ -5,6 +5,9 @@ $(function(){
     ul.append('<li><a href="index.html">Accueil</a></li>');
     ul.append('<li><a href="partenaires.html">Partenaires</a></li>');
     ul.append('<li><a href="tombola.html">Tombola</a></li>');
+    ul.append('<li><a href="planning.html">Planning</a></li>');
+    ul.append('<li><a href="reglements.html">Règlements</a></li>');
+    ul.append('<li><a href="resultats.html">Résultats</a></li>');
     ul.append('<li><a href="faq.html">FAQ</a></li>');
     ul.append('<li><a href="about.html">A propos</a></li>');
 });
