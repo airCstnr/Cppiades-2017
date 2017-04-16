d740a6708ad0b30dab0ec105699a98145de94e36
aircstnr <raphael.castanier@free.fr>
2017-04-16 19:37:14 +0200

    Modification du nav pour un style material avec classe .active_nav

--- nav.js
+++ nav.js
@@ -2,12 +2,21 @@
 
 $(function(){
     var ul = $('ul').first();
-    ul.append('<li><a href="index.html">Accueil</a></li>');
-    ul.append('<li><a href="partenaires.html">Partenaires</a></li>');
-    ul.append('<li><a href="tombola.html">Tombola</a></li>');
-    ul.append('<li><a href="planning.html">Planning</a></li>');
-    ul.append('<li><a href="reglements.html">Règlements</a></li>');
-    ul.append('<li><a href="resultats.html">Résultats</a></li>');
-    ul.append('<li><a href="faq.html">FAQ</a></li>');
-    ul.append('<li><a href="about.html">A propos</a></li>');
+    ul.append('<li><a class="index.html" href="index.html">Accueil</a></li>');
+    ul.append('<li><a class="partenaires.html" href="partenaires.html">Partenaires</a></li>');
+    ul.append('<li><a class="tombola.html" href="tombola.html">Tombola</a></li>');
+    ul.append('<li><a class="planning.html" href="planning.html">Planning</a></li>');
+    ul.append('<li><a class="reglements.html" href="reglements.html">Règlements</a></li>');
+    ul.append('<li><a class="resultats.html" href="resultats.html">Résultats</a></li>');
+    ul.append('<li><a class="faq.html" href="faq.html">FAQ</a></li>');
+    ul.append('<li><a class="about.html" href="about.html">A propos</a></li>');
+    
+    
+    var url = document.URL;
+	var filename = url.substring(url.lastIndexOf('/')+1);
+    if (filename==null) { console.log('filename == null'); filename='index.html';}
+    console.log(filename);
+	var nav = document.getElementsByTagName("nav");
+    var lien = nav[0].getElementsByClassName(filename);
+	lien[0].className = "active_nav";
 });
